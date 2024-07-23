b1026830d4121a1eca0a476f3d82fbda531134cd
sblsreddy <r14141@gmail.com>
2024-07-23 06:57:00 -0500

    Basic types and annotations

---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log('Hello Typescript Basic types');
+
+const myName: string = 'Sridhar Reddy Sangala';
+
+const id: number = 1233;
+const pie: number = 3.1415;
+const negative: number = -1123;
+
+const yes: boolean = true;
+const no: boolean = false;
+
+const noDefinition:undefined = undefined;
+const nothingHere: null = null;
